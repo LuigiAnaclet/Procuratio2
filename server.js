3a399845a589fd47a553a17fc162fee5770bb627
LuigiAnaclet <luigi.anaclet@supinfo.com>
2023-12-31 06:45:01 +0100

    nouveau lien

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://procuratio-lanaclet.francecentral.cloudapp.azure.com:${PORT}`);
 });
 
 
