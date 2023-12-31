07a4a41e6f27458a2ab7dfc1eead980724239f26
LuigiAnaclet <luigi.anaclet@supinfo.com>
2023-12-31 05:16:42 +0000

    Vrai prod

--- database.js
+++ database.js
@@ -2,10 +2,14 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: 'procuratio-server.postgres.database.azure.com',
     database: 'procuratio_db',
     password: 'Stanley97231!',
-    port: 5432
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
 });
 
 module.exports = pool;
